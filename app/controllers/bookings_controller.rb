e58e7db2db7f73784414906cc9c43cf17cb62712
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-12 16:05:46 +0900

    Added controller methods for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,37 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :delete]
+
   def index
     @bookings = Booking.all
-  all
+  end
 
   def show
+    # Don't think this method is required
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.session = @session
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+      # else
+      # render :controller => "studios", :action => :index
+    end
+  end
+
+  def delete
+    @booking.destroy
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def set_booking
     @booking = Booking.find(params[:id])
-  all
+  end
+
+  def booking_params
+    params.require(:booking).permit(:session_id, :user_id)
+  end
 end
