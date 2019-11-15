94458a9564d8dd7ff528c8259f910f911850459a
Sabrina Gheissari <sabrina.gheissari1@gmail.com>
2019-11-15 12:50:09 +0900

    added reviews to the studio controller and the studio show page

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -4,4 +4,5 @@ class Studio < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many :reviews, through: :courses
 end
